b892fc6dc8eadd4f09835d89b8237b7f3c8fea24
kvvzie <161234221+kvvzie@users.noreply.github.com>
2024-03-09 15:34:24 +0100

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,19 +1,16 @@
 import subprocess
 import time
 
-# Ścieżka do pliku skryptu PowerShell
+# script path
 script_path = "updated_script.ps1"
 
-# Komenda PowerShell do uruchomienia skryptu w nowym oknie
+# new powershell comand
 powershell_command = f"Start-Process powershell.exe -ArgumentList '-NoProfile', '-ExecutionPolicy', 'Bypass', '-File', '{script_path}'"
 
-# Liczba iteracji, które chcesz wykonać (25 ms * 400 = 10 s)
+# iterations (25 ms * 400 = 10 s)
 iterations = 400
 
-# Pętla uruchamiająca skrypt co 25 ms przez 10 sekund
+# loop
 for _ in range(iterations):
     subprocess.run(["powershell", "-Command", powershell_command], shell=True)
     time.sleep(0.025)  # Czekaj 25 ms
-
-# Możesz dodać kod, który zaczeka na zakończenie działania PowerShell
-# i zwróci kontrolę użytkownikowi.
